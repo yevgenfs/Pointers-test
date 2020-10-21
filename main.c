a41d48bd254ea8bd38d25d400c4dd99388971029
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-21 23:51:27 +0300

    11-task

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+void swap (int *first, int *second)
+{
+	int swaper = *first;
+	*first = *second;
+	*second = swaper;
+}
+int main(void)
+{
+	int f=12;
+	int s= 0;
+	swap(&f,&s);
+	printf("first = %d second = %d \n",f,s);
+}
