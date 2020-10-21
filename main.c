f727988f922fcc09835c3d4332fbf402fe702c02
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-21 23:26:45 +0300

    7-task

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	int lenght ;
+	printf("input lenght");
+	scanf("%d",&lenght);
+	printf("lenght - %d  \n", lenght);
+	int array[lenght];
+	int *ptr = array;
+	for(int i = 0; i < lenght; i++)
+	{
+		printf("input number %d \n",i);
+		scanf("%d",(ptr+i));
+		printf("element - %d : %d \n", i, *(ptr+i));
+	}
+	
+}
