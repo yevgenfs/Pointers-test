fce760e2f296de2bca50946d760f0e8462d4e338
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-22 23:08:10 +0300

    21-task

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	char alph[26];
+	char *ptr = alph;
+	for(int i = 0; i < 26; i++)
+	{
+		*(ptr+i) = i + 'A';
+	}
+	for(int i = 0; i < 26; i++)
+	{
+		printf("%c ",*(ptr+i));
+	}
+}
