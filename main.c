6fcee6a8ff18f8696407ad4575045ae7a449c441
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-22 12:07:04 +0300

    12-task

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+  int number;
+  int factorial = 1;
+  printf("Enter  number: \n");
+  scanf("%d", &number);
+  for (int i = 1; i <= number; ++i) 
+	{
+    factorial *= i;
+  }
+  printf("Factorial of %d = %d \n", number, factorial);
+}
