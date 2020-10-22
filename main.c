a781c276cab80ce3a63b552aba099fa882059736
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-10-22 13:49:54 +0300

    13-task

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+	char str[20];
+	int  vowels = 0;
+	int  constant = 0;
+	printf("enter str \n");
+	fgets(str, sizeof str, stdin);
+	char *ptr = str;
+	for (int i = 0;*(ptr+i) !='\0';i++)
+	{
+		if (*(ptr+i) == 'a' || *(ptr+i) == 'e' || *(ptr+i) == 'i' || *(ptr+i) == 'u' || *(ptr+i) == 'o')
+		{
+			vowels++;
+		}
+		else
+		{
+			constant++;
+		}
+	}
+	printf("vowels = %d \nconstant = %d \n", vowels, constant);
+	
+}
