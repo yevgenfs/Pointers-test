0172e5fe3caa9dbbc5052f1437a9d52e5fb75fab
yevgenfs <44088277+yevgenfs@users.noreply.github.com>
2020-11-05 19:58:05 +0200

    9-task
    9-task

--- main.c
+++ main.c
@@ -1,24 +1,35 @@
+#include <stdlib.h>
 #include <stdio.h>
 int main(void)
 {
-int first_number = 0;
-int second_number = 0;
-int *first_number_P = &first_number;
-int *second_number_P = &second_number;
-printf("first_number \n");
-scanf("%d",first_number_P);
-printf("second_number \n");
-scanf("%d",second_number_P);
-if(*first_number_P > *second_number_P)
-{
-    printf("first_number max %d \n",*first_number_P);
-}
-else if (*first_number_P == *second_number_P)
-{
-	printf("first_number and  second_number max  \n");
-}
-else
-{
-    printf("second_number max %d \n",*second_number_P);
-}
+	int lenght ;
+	printf("input lenght");
+	scanf("%d",&lenght);
+	printf("lenght - %d  \n", lenght);
+	int array[lenght];
+	int *ptr = malloc(sizeof(array));
+	for(int i = 0; i < lenght; i++)
+	{
+		printf("input number %d \n",i);
+		scanf("%d",(ptr+i));
+		printf("element - %d : %d \n", i, *(ptr+i));
+	}
+	for(int i = 0; i < lenght - 1 ; i++)
+	{
+		for(int j = 0; j < lenght - i -1 ; j++)
+		{
+			if (*(ptr+j)>*(ptr+j+1))
+			{
+				int swap   = *(ptr+j);
+				*(ptr+j)   = *(ptr+j+1);
+				*(ptr+j+1) = swap;
+			}
+		}
+	}
+	printf("sorted array \n");
+	for (int i = 0; i < lenght  ; i++)
+	{
+		printf("element - %d : %d \n", i, *(ptr+i));
+	}
+	printf("The Largest element is - %d  \n",*(ptr + (lenght - 1)));
 }
