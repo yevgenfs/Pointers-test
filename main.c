d1a96aacce02f5c6bc4889281930db435c8031c4
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 16:21:13 +0200

    add white space

--- main.c
+++ main.c
@@ -5,23 +5,23 @@ int main(void)
 {
 	int lenght;
 	printf("input lenght");
-	scanf("%d",&lenght);
+	scanf("%d", &lenght);
 	printf("lenght - %d  \n", lenght);
 	int array[lenght];
 	int *ptr = malloc(sizeof(array));
 
 	for(int i = 0; i < lenght; i++)
 	{
-	  printf("input number %d \n",i);
+	  printf("input number %d \n", i);
 	  scanf("%d",(ptr+i));
 	  printf("element - %d : %d \n", i, *(ptr+i));
 	}
 
-	for(int i = 0; i < lenght - 1 ; i++)
+	for(int i = 0; i < lenght - 1; i++)
 	{
-		for(int j = 0; j < lenght - i -1 ; j++)
+		for(int j = 0; j < lenght - i -1; j++)
 		{
-			if (*(ptr+j)>*(ptr+j+1))
+			if (*(ptr+j) > *(ptr+j+1))
 			{
 				int swap   = *(ptr+j);
 				*(ptr+j)   = *(ptr+j+1);
@@ -32,10 +32,10 @@ int main(void)
 
 	printf("sorted array \n");
 
-	for (int i = 0; i < lenght  ; i++)
+	for (int i = 0; i < lenght; i++)
 	{
 		printf("element - %d : %d \n", i, *(ptr+i));
 	}
 
-	printf("The Largest element is - %d  \n",*(ptr + (lenght - 1)));
+	printf("The Largest element is - %d  \n", *(ptr + (lenght - 1)));
 }
