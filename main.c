86a2394f65f5a05e009e6012df56c98a7dcfe620
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 16:10:25 +0200

    update clean code

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 int main(void)
 {
-	int lenght ;
+	int lenght;
 	printf("input lenght");
-	scanf("%d",&lenght);
+	scanf("%d", &lenght);
 	printf("lenght - %d  \n", lenght);
 	int array[lenght];
 	int *ptr = array;
+
 	for(int i = 0; i < lenght; i++)
 	{
-		printf("input number %d \n",i);
-		scanf("%d",(ptr+i));
+		printf("input number %d \n", i);
+		scanf("%d", (ptr+i));
 		printf("element - %d : %d \n", i, *(ptr+i));
 	}
-	
 }
