18582fe268321129106c891f61516aa802f00277
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 15:58:39 +0200

    update clean code

--- main.c
+++ main.c
@@ -3,25 +3,19 @@
 
 int* max(int *first_num, int *second_num)
 {
-	if(*first_num > *second_num)
-	{
-		return first_num;
-	}
-	else
-	{
-    	return second_num;
-	}
+	(*first_num > *second_num) ? (return first_num) : (return second_num);
 }
 
 int main(void)
 {
-int first_number = 0;
-int second_number = 0;
-int *first_number_P = &first_number;
-int *second_number_P = &second_number;
-printf("first_number \n");
-scanf("%d",first_number_P);
-printf("second_number \n");
-scanf("%d",second_number_P);
-printf("max %d \n",*max(first_number_P, second_number_P));
+	int first_number     = 0;
+	int second_number    = 0;
+	int *first_number_P  = &first_number;
+	int *second_number_P = &second_number;
+
+	printf("first_number \n");
+	scanf("%d", first_number_P);
+	printf("second_number \n");
+	scanf("%d", second_number_P);
+	printf("max %d \n", *max(first_number_P, second_number_P));
 }
