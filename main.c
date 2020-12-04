88987c1a75d750dc3b910988ff161fd30e344e18
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 16:00:13 +0200

    update struct name

--- main.c
+++ main.c
@@ -4,23 +4,23 @@
 typedef struct 
 {
   char name[28];	
-}user;
+}user_t;
 
 typedef union 
 {
   char name[28];	
-}names;
+}names_t;
 
 int main(void)
 {
-	user   users              = {"John Alter from Court Street"};
-	user   admin              = {"Yevhen from Court Street"};
-	user   *ptr               = &users;
-	user   *ptr_d             = &admin;
-	names  name               = {"Jhon Mc Jhon Mc  "};
-	names  *u_ptr             = &name;
-	user   *array[2]          = {ptr,ptr_d};
-	user   (*(*array_ptr)[2]) = &array;
+	user_t   users              = {"John Alter from Court Street"};
+	user_t   admin              = {"Yevhen from Court Street"};
+	user_t   *ptr               = &users;
+	user_t   *ptr_d             = &admin;
+	names_t  name               = {"Jhon Mc Jhon Mc  "};
+	names_t  *u_ptr             = &name;
+	user_t   *array[2]          = {ptr,ptr_d};
+	user_t   (*(*array_ptr)[2]) = &array;
 	printf("pointer %s \n", ptr->name);
 	printf("union %s \n", u_ptr->name);
 	printf("array_ptr %s", (*(*array_ptr+1))->name);
