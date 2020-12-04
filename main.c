dbb10a292083e563896eb4b44f73f79b7a711c8a
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 16:02:38 +0200

    update clean code

--- main.c
+++ main.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 int main(void)
 {
-	int lenght ;
+	int lenght;
 	printf("input lenght");
-	scanf("%d",&lenght);
+	scanf("%d", &lenght);
 	printf("lenght - %d  \n", lenght);
 	int array[lenght];
 	int *ptr = array;
+
 	for(int i = 0; i < lenght; i++)
 	{
-		printf("input number %d \n",i);
-		scanf("%d",(ptr+i));
+		printf("input number %d \n", i);
+		scanf("%d", (ptr+i));
 		printf("element - %d : %d \n", i, *(ptr+i));
 	}
-	for(int i = 0; i < lenght - 1 ; i++)
+
+	for(int i = 0; i < lenght - 1; i++)
 	{
-		for(int j = 0; j < lenght - i -1 ; j++)
+		for(int j = 0; j < lenght - i -1; j++)
 		{
-			if (*(ptr+j)>*(ptr+j+1))
+			if (*(ptr+j) > *(ptr+j+1))
 			{
 				int swap   = *(ptr+j);
 				*(ptr+j)   = *(ptr+j+1);
@@ -26,8 +28,10 @@ int main(void)
 			}
 		}
 	}
+
 	printf("sorted array \n");
-	for (int i = 0; i < lenght  ; i++)
+
+	for (int i = 0; i < lenght; i++)
 	{
 		printf("element - %d : %d \n", i, *(ptr+i));
 	}
