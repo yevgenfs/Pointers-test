4868a07e9a0c18b566aed91beec2c74b1f23f166
yevgenfs <yevgenfs2@gmail.com>
2020-12-04 15:46:45 +0200

    update clean code

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 typedef struct 
 {
   char name[28];	
 }user;
+
 typedef union 
 {
   char name[28];	
@@ -11,15 +13,15 @@ typedef union
 
 int main(void)
 {
-	user users = {"John Alter from Court Street"};
-	user admin = {"Yevhen from Court Street"};
-	user *ptr = &users;
-	user *ptr_d = &admin;
-	names name = {"Jhon Mc Jhon Mc  "};
-	names *u_ptr = &name;
-	user *array[2] = {ptr,ptr_d};
-	user (*(*array_ptr)[2]) = &array;
-	printf("pointer %s \n",ptr->name);
-	printf("union %s \n",u_ptr->name);
-	printf("array_ptr %s",(*(*array_ptr+1))->name);
+	user   users              = {"John Alter from Court Street"};
+	user   admin              = {"Yevhen from Court Street"};
+	user   *ptr               = &users;
+	user   *ptr_d             = &admin;
+	names  name               = {"Jhon Mc Jhon Mc  "};
+	names  *u_ptr             = &name;
+	user   *array[2]          = {ptr,ptr_d};
+	user   (*(*array_ptr)[2]) = &array;
+	printf("pointer %s \n", ptr->name);
+	printf("union %s \n", u_ptr->name);
+	printf("array_ptr %s", (*(*array_ptr+1))->name);
 }
