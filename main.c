959bfe2e91aad98f7eddf371bb8b11209d81b160
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 16:42:12 +0200

    add comment

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* *
+ * @brief program in C to print all the alphabets using a pointer
+ * */
 int main(void)
 {
-	char alph[26];
-	char *ptr = alph;
-	for(int i = 0; i < 26; i++)
-	{
-		*(ptr+i) = i + 'A';
-	}
-	
-	for(int i = 0; i < 26; i++)
-	{
-		printf("%c ",*(ptr+i));
-	}
+  char alph[26];
+  char *ptr = alph;
+  for(int i = 0; i < 26; i++)
+  {
+    *(ptr+i) = i + 'A';
+  }
+
+  for(int i = 0; i < 26; i++)
+  {
+    printf("%c ", *(ptr+i));
+  }
 }
