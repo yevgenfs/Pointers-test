9e16c1da44df2c4512b2d73aad39615fc42b4e0a
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 15:52:47 +0200

    add comment work on the requested changes

--- main.c
+++ main.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* *
+ * @brief program in C to compute the sum of all elements in an array using pointers
+ * */
 int main(void)
 {
-	int lenght ;
-	printf("input lenght");
-	scanf("%d", &lenght);
-	printf("lenght - %d  \n", lenght);
-	int array[lenght];
-	int *ptr = array;
-	int sum = 0;
+  int lenght = 0;
 
-	for(int i = 0; i < lenght; i++)
-	{
-		printf("input number %d \n", i);
-		scanf("%d", (ptr+i));
-		printf("element - %d : %d \n", i, *(ptr+i));
-		sum +=  *(ptr+i);
-	}
+  printf("input lenght");
+  scanf("%d", &lenght);
+  printf("lenght - %d  \n", lenght);
 
-	printf("sum = %d  \n", sum);
+  int array[lenght];
+  int *ptr = array;
+  int sum = 0;
+
+  for(int i = 0; i < lenght; i++)
+  {
+    printf("input number %d \n", i);
+    scanf("%d", (ptr+i));
+    printf("element - %d : %d \n", i, *(ptr+i));
+    sum +=  *(ptr+i);
+  }
+
+  printf("sum = %d  \n", sum);
 }
