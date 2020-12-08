7efc1269c91bf2fb759bcc0908d3098d3211636c
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 14:38:20 +0200

    add a commemt and correct the code

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/* *
+ * @brief program to find the factorial of a given number 
+ * */
 int main(void)
 {
-  int number;
+  int number    = 0;
   int factorial = 1;
+
   printf("Enter  number: \n");
   scanf("%d", &number);
-	
-  for (int i = 1; i <= number; ++i) 
-	{
-    factorial *= i;
-  }
 
-  printf("Factorial of %d = %d \n", number, factorial);
+  if (number > 0)
+  {
+    for (int i = 1; i <= number; ++i) 
+    {
+      factorial *= i;
+    }
+    printf("Factorial of %d = %d \n", number, factorial);
+  }
+  else
+  {
+    printf("Factorial of %d = %d \n", number, 0);
+  }
 }
