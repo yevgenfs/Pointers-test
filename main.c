7dbc09d78601d425f2d13dd5008bf011051bf1f5
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 17:14:05 +0200

    add comment work on the requested changes

--- main.c
+++ main.c
@@ -1,41 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* *
+ * @brief program in C to find the largest element using Dynamic Memory Allocation
+ * */
 int main(void)
 {
-	int lenght;
-	printf("input lenght");
-	scanf("%d", &lenght);
-	printf("lenght - %d  \n", lenght);
-	int array[lenght];
-	int *ptr = malloc(sizeof(array));
+  int lenght = 0;
 
-	for(int i = 0; i < lenght; i++)
-	{
-	  printf("input number %d \n", i);
-	  scanf("%d",(ptr+i));
-	  printf("element - %d : %d \n", i, *(ptr+i));
-	}
+  printf("input lenght");
+  scanf("%d", &lenght);
+  printf("lenght - %d  \n", lenght);
 
-	for(int i = 0; i < lenght - 1; i++)
-	{
-		for(int j = 0; j < lenght - i -1; j++)
-		{
-			if (*(ptr+j) > *(ptr+j+1))
-			{
-				int swap   = *(ptr+j);
-				*(ptr+j)   = *(ptr+j+1);
-				*(ptr+j+1) = swap;
-			}
-		}
-	}
+  int array[lenght];
+  int *ptr;
 
-	printf("sorted array \n");
+  if((ptr = malloc(sizeof(array))) != NULL)
+  {
+    for(int i = 0; i < lenght; i++)
+    {
+      printf("input number %d \n", i);
+      scanf("%d", (ptr + i));
+      printf("element - %d : %d \n", i, *(ptr + i));
+    }
 
-	for (int i = 0; i < lenght; i++)
-	{
-		printf("element - %d : %d \n", i, *(ptr+i));
-	}
+    int swap = 0;
 
-	printf("The Largest element is - %d  \n", *(ptr + (lenght - 1)));
+    for(int i = 0; i < lenght - 1; i++)
+    {
+      for(int j = 0; j < lenght - i - 1; j++)
+      {
+        if (*(ptr + j) > *(ptr + j + 1))
+        {
+          swap           = *(ptr + j);
+          *(ptr + j)     = *(ptr + j + 1);
+          *(ptr + j + 1) = swap;
+        }
+      }
+    }
+
+    printf("sorted array \n");
+
+    for (int i = 0; i < lenght; i++)
+    {
+      printf("element - %d : %d \n", i, *(ptr + i));
+    }
+
+    printf("The Largest element is - %d  \n", *(ptr + (lenght - 1)));
+  }
 }
