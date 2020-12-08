cb506f412cfc70c66f914cbbf71c87827759e0e3
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 16:31:07 +0200

    add comment mork on the requested changes

--- main.c
+++ main.c
@@ -1,35 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* *
+ * @brief program in C to print the elements of an array in reverse order
+ * */
 int main(void)
 {
-	int lenght;
-	printf("input lenght");
-	scanf("%d", &lenght);
-	printf("lenght - %d  \n", lenght);
-	int array[lenght];
-	int *ptr = array;
+  int lenght = 0;
 
-	for(int i = 0; i < lenght; i++)
-	{
-		printf("input number %d \n", i);
-		scanf("%d", (ptr+i));
-		printf("element - %d : %d \n", i, *(ptr+i));
-	}
+  printf("input lenght");
+  scanf("%d", &lenght);
+  printf("lenght - %d  \n", lenght);
 
-	int lenght_counter = lenght;
+  int array[lenght];
+  int *ptr = array;
 
-	for (int j = 0 ; j<lenght/2 ; j++,lenght_counter--)
-	{
-		char temp               = *(ptr+j);
-		*(ptr+j)                = *(ptr+lenght_counter-1);
-		*(ptr+lenght_counter-1) = temp;
-	}
+  for(int i = 0; i < lenght; i++)
+  {
+    printf("input number %d \n", i);
+    scanf("%d", (ptr+i));
+    printf("element - %d : %d \n", i, *(ptr+i));
+  }
 
-	printf("reversed");
+  int lenght_counter = lenght;
+  char temp = ""; 
 
-	for(int i = 0; i < lenght; i++)
-	{
-		printf("element - %d : %d \n", i, *(ptr+i));
-	}
+  for (int j = 0; j<lenght/2; j++)
+  {
+    temp                    = *(ptr+j);
+    *(ptr+j)                = *(ptr+lenght_counter-1);
+    *(ptr+lenght_counter-1) = temp;
+    lenght_counter--;
+  }
+
+  printf("reversed");
+
+  for(int i = 0; i < lenght; i++)
+  {
+    printf("element - %d : %d \n", i, *(ptr+i));
+  }
 }
