0659075fd5acf00c16fe95285cbe7679e25e4a19
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 17:27:52 +0200

    add comment

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* *
+ * @brief program in C to swap elements using call by reference
+ * */
 void swap (int *first, int *second)
 {
-	int swaper = *first;
-	*first     = *second;
-	*second    = swaper;
+  int swaper = *first;
+  *first     = *second;
+  *second    = swaper;
 }
 
 int main(void)
 {
-	int f = 12;
-	int s = 0;
-	swap(&f, &s);
-	printf("first = %d second = %d \n", f, s);
+  int f = 12;
+  int s = 0;
+  swap(&f, &s);
+  printf("first = %d second = %d \n", f, s);
 }
