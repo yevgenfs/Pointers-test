1befa1f5b3d74764e7efd80107e87df20b764b2e
yevgenfs <yevgenfs2@gmail.com>
2020-12-08 16:39:18 +0200

    add comment work on the requested changes

--- main.c
+++ main.c
@@ -1,27 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_NAME_LENGHT 28
+
+/* *
+ * @brief program in C to show the usage of pointer to structure , union and 
+ * program in C to show a pointer to an array which contents are pointer to structure
+ * */
 typedef struct 
 {
-  char name[28];	
+  char name[MAX_NAME_LENGHT];	
 }user_t;
 
 typedef union 
 {
-  char name[28];	
+  char name[MAX_NAME_LENGHT];	
 }names_t;
 
 int main(void)
 {
-	user_t   users              = {"John Alter from Court Street"};
-	user_t   admin              = {"Yevhen from Court Street"};
-	user_t   *ptr               = &users;
-	user_t   *ptr_d             = &admin;
-	names_t  name               = {"Jhon Mc Jhon Mc  "};
-	names_t  *u_ptr             = &name;
-	user_t   *array[2]          = {ptr,ptr_d};
-	user_t   (*(*array_ptr)[2]) = &array;
-	printf("pointer %s \n", ptr->name);
-	printf("union %s \n", u_ptr->name);
-	printf("array_ptr %s", (*(*array_ptr+1))->name);
+  user_t   users              = {"John Alter from Court Street"};
+  user_t   admin              = {"Yevhen from Court Street"};
+  user_t   *ptr               = &users;
+  user_t   *ptr_d             = &admin;
+  names_t  name               = {"Jhon Mc Jhon Mc  "};
+  names_t  *u_ptr             = &name;
+  user_t   *array[2]          = {ptr,ptr_d};
+  user_t   (*(*array_ptr)[2]) = &array;
+
+  printf("pointer %s \n", ptr->name);
+  printf("union %s \n", u_ptr->name);
+  printf("array_ptr %s", (*(*array_ptr+1))->name);
 }
